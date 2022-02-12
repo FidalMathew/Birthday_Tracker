890ec6f0d4313dfea83a353f2afa5a4633a482df
Fidal <fidal15perfect@gmail.com>
2022-02-12 19:23:53 +0530

    minor changes

--- client/src/Components/Register.jsx
+++ client/src/Components/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { axiosInstance } from '../config';
 
 import "./Register.css"
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 export default function Register() {
 
 
@@ -12,8 +12,6 @@ export default function Register() {
     })
 
     const [error, setError] = useState(false);
-
-    const history = useNavigate();
 
     let name, value;
     const HandleInputs = (e) => {
